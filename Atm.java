33eb25c3ace716007dde99bea3b3d1be24125386
WilliamAbraham <williamjohnabraham1@gmail.com>
2023-08-31 10:36:29 -0700

    withdraw + tester

--- Atm.java
+++ Atm.java
@@ -36,5 +36,47 @@ public class Atm{
             throw new IllegalArgumentException("You're broke AF");
         }
     }
+
+    public Double withdrawMoney(String userID, double amount){
+        if ((accounts.get(userID) - amount) >= 0){
+            accounts.put(userID, accounts.get(userID) - amount);
+            return accounts.get(userID);
+        } else {
+            throw new IllegalArgumentException("You're broke AF");
+        }
+    }
+
+    public static void main(String[] args) {
+        Atm atm = new Atm();
+
+        try {
+            atm.openAccount("user123", 100.0);
+            System.out.println("Account opened successfully.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Failed to open account: " + e.getMessage());
+        }
+
+        try {
+            atm.depositMoney("user123", 50.0);
+            System.out.println("Deposit successful. New balance: " + atm.checkBalance("user123"));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Failed to deposit money: " + e.getMessage());
+        }
+
+        try {
+            double withdrawalAmount = 70.0;
+            atm.withdrawMoney("user123", withdrawalAmount);
+            System.out.println("Withdrawal of " + withdrawalAmount + " successful. New balance: " + atm.checkBalance("user123"));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Failed to withdraw money: " + e.getMessage());
+        }
+
+        try {
+            atm.closeAccount("user123");
+            System.out.println("Account closed successfully.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Failed to close account: " + e.getMessage());
+        }
+    }
 }
 
