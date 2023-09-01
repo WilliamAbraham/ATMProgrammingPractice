c5f80e750ad9472a41cb6dae4ae34773f55af8f6
WilliamAbraham <williamjohnabraham1@gmail.com>
2023-09-01 12:01:05 -0700

    tester

--- Atm.java
+++ Atm.java
@@ -79,10 +79,45 @@ public class Atm{
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         Atm atm = new Atm();
 
-        
+        // Open accounts
+        atm.openAccount("Alice", 1000.0);
+        atm.openAccount("Bob", 500.0);
+
+        // Deposit money
+        atm.depositMoney("Alice", 200.0);
+        atm.depositMoney("Bob", 300.0);
+
+        System.out.println("Alice: " + atm.checkBalance("Alice"));
+        System.out.println("Bob: " + atm.checkBalance("Bob"));
+
+        // Withdraw money
+        atm.withdrawMoney("Alice", 50.0);
+        atm.withdrawMoney("Bob", 100.0);
+
+        System.out.println("Alice: " + atm.checkBalance("Alice"));
+        System.out.println("Bob: " + atm.checkBalance("Bob"));
+
+        // Transfer money
+        boolean transferSuccess = atm.transferMoney("Alice", "Bob", 200.0);
+
+        if (transferSuccess) {
+            System.out.println("Money transferred successfully.");
+        } else {
+            System.out.println("Money transfer failed.");
+        }
+
+        System.out.println("Alice: " + atm.checkBalance("Alice"));
+        System.out.println("Bob: " + atm.checkBalance("Bob"));
+        System.out.println("transfer was " + transferSuccess);
+
+        // Close accounts
+        // atm.closeAccount("Alice");
+        // atm.closeAccount("Bob");
+
+        atm.audit();
     }
 }
 
